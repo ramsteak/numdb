d8802f21e314e2b41944cc0680049e6fdd2d684f
Marco Stecca <marco.stecca99@gmail.com>
2024-04-02 18:51:37 +0200

    Added filetypes and reader Handles different filetypes by registering extension and reader

---
+++ src/numdb/spectrum/filetypes.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+from pathlib import Path
+from pandas import Series
+from .spectrum import Spectrum, XAxisType
+from typing import Callable
+
+_registered_filetypes: dict[str, FileType] = {}
+
+class FileType:
+    def __init__(
+        self,
+        type_name: str,
+        allowed_extensions: list[str] | None,
+        read_method: Callable[[Path, str, Spectrum], tuple[Series, XAxisType] | None],
+        meta_method: Callable[[Path, str], tuple[Series] | None],
+        accept_extension_rule: Callable[[str], bool] | None = None,
+    ) -> None:
+        self.name = type_name
+
+        self.allowed_extensions = allowed_extensions
+
+        self.read_method = read_method
+
+        if accept_extension_rule is None:
+
+            def _aer(e: str, /) -> bool:
+                if self.allowed_extensions is None:
+                    return False
+                return e in self.allowed_extensions
+
+            self.accept_extension_rule = _aer
+
+        elif callable(accept_extension_rule):
+            self.accept_extension_rule = accept_extension_rule
+        else:
+            raise ValueError("Invalid accept extension rule")
+
+
+class KeyCollisionError(Exception):
+    ...
+
+
+def register_filetype(ftype: FileType, *, overwrite: bool = False) -> None:
+    if not overwrite:
+        if ftype.name in _registered_filetypes:
+            raise KeyCollisionError(f'Key "{ftype.name}" already exists')
+
+    _registered_filetypes[ftype.name] = ftype
+
+
+def get_filetypes():
+    return _registered_filetypes.keys()
---
+++ src/numdb/spectrum/reader.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+from pathlib import Path
+from pandas import Series, Index
+from .spectrum import Spectrum, XAxisType
+from ..exceptions import ReadError
+from .filetypes import FileType, _registered_filetypes
+from numpy import typing as npt
+import numpy as np
+from scipy.interpolate import interp1d
+
+
+def _read_auto(
+    fp: Path, name: str, mode=Spectrum.AB, filetype: None | str | FileType = None, **kw
+) -> tuple[Series, XAxisType]:
+    ext = fp.suffix.lower()
+
+    if filetype is None or filetype == "auto":
+        for _, ftype in _registered_filetypes.items():
+            if ftype.accept_extension_rule(ext):
+                read_result = ftype.read_method(fp, name, mode, **kw)
+                if read_result is None:
+                    continue
+
+                spectrum, xaxistype = read_result
+                break
+        else:
+            raise ReadError("Unable to detect filetype")
+
+    elif isinstance(filetype, (FileType, str)):
+        if isinstance(filetype, str):
+            filetype = _registered_filetypes[filetype]
+
+        if filetype.accept_extension_rule(ext):
+            read_result = filetype.read_method(fp, name, mode, **kw)
+            if read_result is None:
+                raise ReadError("File was unrecognized")
+
+            spectrum, xaxistype = read_result
+
+    else:
+        raise ReadError("Invalid filetype specification")
+
+    return spectrum, xaxistype
+
+
+def read_spectrum(
+    fp: Path,
+    name: str,
+    mode: Spectrum = Spectrum.AB,
+    xaxis: XAxisType = XAxisType.Unknown,
+    roundx: int | None = 5,
+    roundy: int | None = 5,
+    filetype: FileType | str = "auto",
+    xinterp: npt.ArrayLike | None = None,
+    interp_kind: str = "linear",
+    **kw,
+) -> Series:
+    s, curr_xaxis = _read_auto(fp, name, mode, filetype, **kw)
+
+    # X axis conversions
+    curr_xexp = curr_xaxis.value
+    desr_xexp = xaxis.value
+
+    X = s.index
+
+    if curr_xexp != desr_xexp:
+        # unit inversion
+        if curr_xexp > 50 and curr_xexp < 50:
+            X = 1 / X
+            curr_xexp -= 100
+        elif curr_xexp < 50 and curr_xexp > 50:
+            X = 1 / X
+            curr_xexp += 100
+
+        # unit alignment
+        if curr_xexp != desr_xexp:
+            diff = -(desr_xexp - curr_xexp)
+            X *= 10**diff
+
+    # X axis interpolation
+    if xinterp is not None:
+        interp = interp1d(s.index.values, s.values, kind=interp_kind, fill_value="extrapolate")  # type: ignore
+        s = Series(interp(xinterp), index=Index(xinterp), name=s.name)  # type: ignore
+
+    # X, Y axis rounding
+    if roundx is not None:
+        s.index = np.round(s.index, roundx)  # type: ignore
+    if roundy is not None:
+        s = s.round(roundy)
+
+    return s
