2230aef528d0e93e40a69addf4f42f89786dd5d8
Marco Stecca <marco.stecca99@gmail.com>
2024-04-12 19:55:07 +0200

    Corrected typo: importing local file

--- src/numdb/spectrum/ftypereader/__init__.py
+++ src/numdb/spectrum/ftypereader/__init__.py
@@ -2,10 +2,10 @@ from ..filetypes import register_filetype
 
 _FTYPEREADER = "_FTYPEREADER"
 
-from csv_innospectra import innospectra_ftype
+from .csv_innospectra import innospectra_ftype
 
 register_filetype(innospectra_ftype)
 
-from opus_bruker import opus_ftype
+from .opus_bruker import opus_ftype
 
 register_filetype(opus_ftype)
