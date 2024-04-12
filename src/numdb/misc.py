a57d38bbfc770703a466eafa3799515744710f47
Marco Stecca <marco.stecca99@gmail.com>
2024-04-12 19:54:30 +0200

    Added flatten_dict

--- src/numdb/misc.py
+++ src/numdb/misc.py
@@ -1,6 +1,4 @@
-from typing import TypeVar
-from typing import Mapping
-from typing import Any
+from typing import TypeVar, Mapping, Any, Iterable
 
 _T = TypeVar("_T")
 _K = TypeVar("_K")
@@ -38,3 +36,35 @@ def get_first(
         if default is ...:
             raise ValueError(f"Property {property} not defined")
         return default
+
+
+def flatten_dict(
+    nested_dict: dict[str, Any | dict[str, Any]], *, join_char: str = "."
+) -> dict[str, Any]:
+    """Flattens a nested dictionary.
+    The function takes a nested dict[str, Any] (where the keys are strings) and
+    flattens it to a dict where the keys are composed by joining the keys of the
+    nested dicts with `join_char`. Values that are not of type dict are preserved.
+
+    Args:
+        `nested_dict`: the dictionaty to flatten. Keys of all nested dicts should be str.
+        `join_char`: optional, the character used to join the keys when flattening.
+            default: "."
+
+    Returns:
+        `dict`: The flattened dictionary
+
+    Example:
+        >>> nested_dict = {'a': 1, 'b': {'c': 2, 'd': {'e': 3}}}
+        >>> flatten_dict(nested_dict)
+        {'a': 1, 'b.c': 2, 'b.d.e': 3}
+    """
+
+    def walk(_d: dict, prev: tuple[str, ...]) -> Iterable[tuple[tuple[str, ...], Any]]:
+        for k, v in _d.items():
+            if isinstance(v, dict):
+                yield from walk(v, (*prev, k))
+            else:
+                yield ((*prev, k), v)
+
+    return {join_char.join(ks): v for ks, v in walk(nested_dict, ())}
