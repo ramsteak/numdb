0e6c5bc706c1a44563e2862a4160889ca412e7bb
Marco Stecca <marco.stecca99@gmail.com>
2024-04-17 11:34:31 +0200

    Revert "Added get_first check for ignore_errors"
    This reverts commit e5d66a9273824523d5dc9a00118bde8a17ae272c.

--- numdb/loader/loader.py
+++ numdb/loader/loader.py
@@ -13,7 +13,7 @@ _load_defaults: dict[str, Any] = {}
 
 
 def import_files(
-    *npaths: str, root: Path | None = None, ignore_errors: bool | None = None, **kw
+    *npaths: str, root: Path | None = None, ignore_errors: bool = False, **kw
 ) -> tuple[DataFrame, DataFrame]:
     root = get_first("root", root, _load_defaults)
 
@@ -31,7 +31,6 @@ def import_files(
             spectra.append(s)
             metadata.append(m)
         except ReadError:
-            get_first("ignore_errors", ignore_errors, _load_defaults, default=False)
             if not ignore_errors:
                 raise
 
